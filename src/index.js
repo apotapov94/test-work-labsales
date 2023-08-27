63fffd1986e97c599bfb3367f7f6fb7c3d81f572
apotapov94 <62658363+apotapov94@users.noreply.github.com>
2023-08-27 19:59:03 +0300

    футер и слайдер готовы

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import "./js/common";
 import "./scss/main.scss";
 import "./css/main.css";
+
+if ($(window).width() < 992) {
+  $(".swiper-wrapper").removeClass("gallery__slider");
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 3,
+  });
+}
